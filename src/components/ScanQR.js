ebd0a3b42bbcd8f16af643dfa47859aa1b47463c
abhi <abhibubane12@gmail.com>
2025-12-01 11:16:13 +0530

    vercel

--- src/components/ScanQR.js
+++ src/components/ScanQR.js
@@ -68,7 +68,7 @@ const ScanQR = () => {
 
       try {
         // IMPORTANT: Replace this with your actual Google Apps Script Web App URL
-        const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/library/d/1p3fscSVbKaejOkgRNLy1uS0iCEzplf6SGsLhgH8WNSioPhnbHjAH9L9G/1'; // Make sure this is your deployed URL
+        const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzAeH9iwegHgozHv06KrfNVeftgOPLI_Nkr0-Zn3WSABKYg0zjiUuSIeTgIQFnqrcRH/exec'; // Make sure this is your deployed URL
 
         // We can now use standard fetch without 'no-cors' to read the response
         const response = await fetch(GOOGLE_SCRIPT_URL, {
